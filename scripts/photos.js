a92177f092f9504dc8b839af3d2d51c8483e8957
Miles Smith <miles.smith010@gmail.com>
2020-07-19 11:36:43 -0700

    upda

--- scripts/photos.js
+++ scripts/photos.js
@@ -1,11 +1,8 @@
 var moreImages = [
-  "http://lorempixel.com/72/72/",
-  "http://lorempixel.com/72/72/",
-  "http://lorempixel.com/72/72/",
-  "http://lorempixel.com/72/72/",
-  "http://lorempixel.com/72/72/",
-  "http://lorempixel.com/72/72/",
-  "http://lorempixel.com/72/72/"
+  "https://res.cloudinary.com/milessmith/image/upload/v1591902620/Personal%20Site/IMG_9563-2.jpg",
+  "https://res.cloudinary.com/milessmith/image/upload/v1591902620/Personal%20Site/IMG_9563-2.jpg",
+    "https://res.cloudinary.com/milessmith/image/upload/v1591902620/Personal%20Site/IMG_9563-2.jpg",
+    "https://res.cloudinary.com/milessmith/image/upload/v1591902620/Personal%20Site/IMG_9563-2.jpg",
 ];
 
 $("#more").on("click", function(){
