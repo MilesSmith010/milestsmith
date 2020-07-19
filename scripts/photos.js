4be0deb8edf331dc58271d18c2f7f212edad060e
Miles Smith <miles.smith010@gmail.com>
2020-07-19 11:40:38 -0700

    Update photos.js

--- scripts/photos.js
+++ scripts/photos.js
@@ -1,4 +1,5 @@
-var moreImages = [
+window.onload = function(){ 
+   var moreImages = [
   "https://res.cloudinary.com/milessmith/image/upload/v1591902620/Personal%20Site/IMG_9563-2.jpg",
   "https://res.cloudinary.com/milessmith/image/upload/v1591902620/Personal%20Site/IMG_9563-2.jpg",
     "https://res.cloudinary.com/milessmith/image/upload/v1591902620/Personal%20Site/IMG_9563-2.jpg",
@@ -13,3 +14,4 @@ $("#more").on("click", function(){
     target.append("<li><img src=\"" + item + "\" /></li>");
   });
 });
+};
