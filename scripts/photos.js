26ec3f365482775485325fbf4383916bad80e8d2
Miles Smith <miles.smith010@gmail.com>
2020-07-19 11:32:08 -0700

    te

--- scripts/photos.js
+++ scripts/photos.js
@@ -1,43 +1,18 @@
-var links = [
-    "https://galleria.io/wp-content/themes/galleria/dist/images/demo/4m.jpg",
-  "https://images.freeimages.com/images/large-previews/851/poppies-1369329.jpg",
-  "https://galleria.io/wp-content/themes/galleria/dist/images/demo/4m.jpg",
-  "https://galleria.io/wp-content/themes/galleria/dist/images/demo/4m.jpg",
-  "https://images.freeimages.com/images/large-previews/fe6/midsummer-fields-1-1394719.jpg",
-  "https://galleria.io/wp-content/themes/galleria/dist/images/demo/4m.jpg",
-  "https://images.freeimages.com/images/large-previews/fe6/midsummer-fields-1-1394719.jpg",
-  "https://galleria.io/wp-content/themes/galleria/dist/images/demo/4m.jpg",
-  "https://images.freeimages.com/images/large-previews/851/poppies-1369329.jpg",
-  "https://images.freeimages.com/images/large-previews/851/poppies-1369329.jpg",
-  "https://images.freeimages.com/images/large-previews/851/poppies-1369329.jpg",
+var moreImages = [
+  "http://lorempixel.com/72/72/",
+  "http://lorempixel.com/72/72/",
+  "http://lorempixel.com/72/72/",
+  "http://lorempixel.com/72/72/",
+  "http://lorempixel.com/72/72/",
+  "http://lorempixel.com/72/72/",
+  "http://lorempixel.com/72/72/"
 ];
 
-var $gallery  = $(".lightgallery1").first();
-var $loadMore = $("#load");
+$("#more").on("click", function(){
+  if (moreImages.length === 0) { return; }
+  var target = $("#imageContainer");
 
-function loadMore(e) {
-  
-  for(var i = 0; i < 3; i++) {
-     
-    if(links.length) {
-
-      var src   = links.pop();
-      var $link = $("<a>");
-      var $img  = $("<img>");
-
-      $img.attr("src", src);
-      $link.attr("href", src).addClass("item");
-      
-      $link.append($img);
-      $gallery.append($link);
-
-    } else {
-      $loadMore.hide();
-    }
-    
-  }
-  
-}
-
-$loadMore.on("click", loadMore);
-loadMore();
+  moreImages.splice(0,3).forEach(function(item){
+    target.append("<li><img src=\"" + item + "\" /></li>");
+  });
+});
